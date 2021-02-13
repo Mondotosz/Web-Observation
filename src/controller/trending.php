f483a049cd504ec0c3b4e15f97f0d0b6e4c971db
Mondotosz <55015209+Mondotosz@users.noreply.github.com>
2021-02-13 12:25:38 +0100

    implemented getPosts to trending controlelr

--- src/controller/trending.php
+++ src/controller/trending.php
@@ -3,8 +3,25 @@
 function trending()
 {
     require_once "view/content/components/cards.php";
+    require_once "model/postsManager.php";
+
+    // get every existing posts
+    // TODO: add a limit
+    $posts = getPosts();
+
+    // initialize an empty card array
     $cards = [];
-    //example data until we have a model
+
+    // creates a card with each post title and first picture
+    foreach ($posts as $post) {
+        // verify if the necessary values exist
+        // might become obsolete with good constraints in the model
+        if (!empty($post["title"]) && !empty($post["pictures"][0]["path"])) {
+            array_push($cards, getComponent($post["title"], $post["pictures"][0]["path"]));
+        }
+    }
+
+    //dummy data to test masonry
     array_push($cards, getComponent("bettraves", "/view/content/img/Betteraves_1600.jpg"));
     array_push($cards, getComponent("faucon", "/view/content/img/Faucon_Crécerelle3.jpg"));
     array_push($cards, getComponent("Bouquetin", "/view/content/img/Bouquetin_fenestral.jpg"));
@@ -14,5 +31,7 @@ function trending()
     array_push($cards, getComponent("Lagopede", "/view/content/img/Lagopèdes_1600.jpg"));
     array_push($cards, getComponent("Patrouille", "/view/content/img/Patrouille_1600.jpg"));
     array_push($cards, getComponent("60CM", "/view/content/img/60cm_1600.jpg"));
+
+    // send cards to the trending view
     require_once "view/trending.php";
 }
