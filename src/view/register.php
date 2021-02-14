5cdc3fee2ace3f038299e6fd24f7b60f45523b36
Mondotosz <55015209+Mondotosz@users.noreply.github.com>
2021-02-14 12:51:41 +0100

    modified headers and readded register

--- src/view/register.php
+++ src/view/register.php
@@ -7,14 +7,18 @@
  */
 
 ob_start();
-$title = "login";
+$title = "register";
 
 ?>
-<?php/** this div is used to center and resize the login form */?>
+<?php/** this div is used to center and resize the register form */?>
 <div class="row w-auto mx-0 px-2 pt-5">
     <?php/** dummy div */?>
     <div class="col-0 col-md-3 col-xl-4"></div>
-    <form class="border rounded-2 p-4 col-12 col-md-6 col-xl-4" method="post" action="/login">
+    <form class="border rounded-2 p-4 col-12 col-md-6 col-xl-4" method="post" action="/register">
+        <div class="mb-3">
+            <label for="inputEmail" class="form-label">Email</label>
+            <input type="email" class="form-control" id="inputEmail" name="inputEmail">
+        </div>
         <div class="mb-3">
             <label for="inputUsername" class="form-label">Username</label>
             <input type="text" class="form-control" id="inputUsername" name="inputUsername">
@@ -23,11 +27,12 @@ $title = "login";
             <label for="inputPassword" class="form-label">Password</label>
             <input type="password" class="form-control" id="inputPassword" name="inputPassword">
         </div>
-        <button type="submit" class="btn btn-primary">Login</button>
+        <button type="submit" class="btn btn-primary">Register</button>
     </form>
     <?php/** dummy div */?>
     <div class="col-0 col-md-3 col-xl-4"></div>
 </div>
+
 <?php
 
 $content = ob_get_clean();
