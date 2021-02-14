aa523c4dd8adc6192497fcd34da45ec29992a502
Mondotosz <55015209+Mondotosz@users.noreply.github.com>
2021-02-14 12:06:42 +0100

    Stylized login/register

--- src/view/login.php
+++ src/view/login.php
@@ -11,17 +11,24 @@ ob_start();
 $title = "login";
 
 ?>
-<form method="post" action="/login">
-    <div class="mb-3">
-        <label for="inputUsername" class="form-label">Username</label>
-        <input type="text" class="form-control" id="inputUsername" name="inputUsername">
-    </div>
-    <div class="mb-3">
-        <label for="inputPassword" class="form-label">Password</label>
-        <input type="password" class="form-control" id="inputPassword" name="inputPassword">
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+<?php/** this div is used to center and resize the login form */?>
+<div class="row w-auto mx-0 px-2 pt-5">
+    <?php/** dummy div */?>
+    <div class="col-0 col-md-3 col-xl-4"></div>
+    <form class="border rounded-2 p-4 col-12 col-md-6 col-xl-4" method="post" action="/login">
+        <div class="mb-3">
+            <label for="inputUsername" class="form-label">Username</label>
+            <input type="text" class="form-control" id="inputUsername" name="inputUsername">
+        </div>
+        <div class="mb-3">
+            <label for="inputPassword" class="form-label">Password</label>
+            <input type="password" class="form-control" id="inputPassword" name="inputPassword">
+        </div>
+        <button type="submit" class="btn btn-primary">Login</button>
+    </form>
+    <?php/** dummy div */?>
+    <div class="col-0 col-md-3 col-xl-4"></div>
+</div>
 <?php
 
 $content = ob_get_clean();
