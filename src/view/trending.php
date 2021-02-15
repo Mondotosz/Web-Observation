857f8b18cd8d18a69a1ed90ada5fcad362e43111
Mondotosz <55015209+Mondotosz@users.noreply.github.com>
2021-02-15 08:13:25 +0100

    Removed useless classes

--- src/view/trending.php
+++ src/view/trending.php
@@ -19,7 +19,7 @@ $scripts = "<script src=\"https://cdn.jsdelivr.net/npm/masonry-layout@4.2.2/dist
     if (!empty($cards)) {
         foreach ($cards as $card) {
     ?>
-            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 gy-3 gx-3 masonry-item">
+            <div class="col-12 col-md-6 col-lg-4 col-xl-3 gy-3 gx-3 masonry-item">
                 <?= $card ?>
             </div>
     <?php
