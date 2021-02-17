a7e01d90b4929885b76dc1b99c1366e1534a901b
FONTANA Benjamin <Benjamin.FONTANA@cpnv.ch>
2021-02-17 11:18:21 +0100

    removed useless examples

--- src/controller/trending.php
+++ src/controller/trending.php
@@ -20,17 +20,7 @@ function trending()
             array_push($cards, getComponent($post["title"], $post["pictures"][0]["path"], "/post/" . $key));
         }
     }
-
-    array_push($cards, getComponent("bettraves", "/view/content/img/Betteraves_1600.jpg","#"));
-    array_push($cards, getComponent("faucon", "/view/content/img/Faucon_Crécerelle3.jpg","#"));
-    array_push($cards, getComponent("Bouquetin", "/view/content/img/Bouquetin_fenestral.jpg","#"));
-    array_push($cards, getComponent("Hermine", "/view/content/img/Hermine_profil_1600.jpg","#"));
-    array_push($cards, getComponent("Arret", "/view/content/img/Raide_1600.jpg","#"));
-    array_push($cards, getComponent("Renard", "/view/content/img/Yeux_renard_1600.jpg","#"));
-    array_push($cards, getComponent("Lagopede", "/view/content/img/Lagopèdes_1600.jpg","#"));
-    array_push($cards, getComponent("Patrouille", "/view/content/img/Patrouille_1600.jpg","#"));
-    array_push($cards, getComponent("60CM", "/view/content/img/60cm_1600.jpg","#"));
-
+    
     // send cards to the trending view
     require_once "view/trending.php";
 }
